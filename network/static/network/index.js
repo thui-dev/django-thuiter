afec61f5dc20736abcd62c8314bfb15ff1fecf73
thui <tuthui.dev@gmail.com>
2024-05-14 22:05:14 -0300

    estragos teste branch

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -15,20 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         load_feed('all', 'feed');
     }/*else if(pathname[1] == 'chats'){
         chats_view();
-    }*/else{
-        load_profile(pathname[1]);
-    }
-    //messages_view();
-    //load_feed('all', 'feed');
-
-    //theme toggle button
-    let mode_toggle = document.querySelector('#mode-toggle');
-    if (localStorage.getItem('theme') == 'light'){
-        mode_toggle.innerHTML = '<i class="bi bi-moon"></i>';
-    }else{
-        mode_toggle.innerHTML = '<i class="bi bi-sun"></i>';
-    }
-    mode_toggle.addEventListener('click', () => {
+    }*/else{ck', () => {
         if (localStorage.getItem('theme') == "light"){
             mode_toggle.innerHTML = '<i class="bi bi-sun"></i>';
             document.querySelector('html').setAttribute('data-bs-theme','dark');
